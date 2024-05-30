5edbad98663aef24cc2eb1c0d15360cb8d0ad81d
Vagif Khalilov <vakhalilov@ozon.ru>
2024-05-31 01:09:20 +0300

    cb as proxy

--- circuit-breaker/app/Main.hs
+++ circuit-breaker/app/Main.hs
@@ -1,22 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import System.Random
 import Network.Wai
-import Network.HTTP.Types
+import qualified Network.Wai as Wai
 import Network.Wai.Handler.Warp (run)
-import Control.Concurrent
-import Data.Time.Clock (getCurrentTime)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import qualified Network.HTTP.Client as HTTP
 import Lib 
+import qualified Data.ByteString.Char8 as BS
 
-app :: Application
-app _ respond = do
-    randomNum <- randomRIO (0, 99) :: IO Int
-    if randomNum < 30
-        then respond $ responseLBS status429 [] "Internal Server Error"
-        else respond $ responseLBS status200 [] "Hello, World!"
+targetServiceUrl :: String
+targetServiceUrl = "http://localhost:8090" 
+
+proxyApp :: Manager -> Application
+proxyApp manager req respond = do
+    let targetUrl = targetServiceUrl ++ BS.unpack (Wai.rawPathInfo req) ++ BS.unpack (Wai.rawQueryString req)
+    initReq <- parseRequest targetUrl
+    body <- Wai.strictRequestBody req
+    let reqToSend = initReq {
+            HTTP.method = Wai.requestMethod req,
+            HTTP.requestHeaders = Wai.requestHeaders req,
+            HTTP.requestBody = HTTP.RequestBodyLBS body
+        }
+    response <- httpLbs reqToSend manager
+    respond $ responseLBS (HTTP.responseStatus response) (HTTP.responseHeaders response) (HTTP.responseBody response)
 
 main :: IO ()
 main = do
+    manager <- newManager tlsManagerSettings
     cb <- initState 1 5 20.0 10 75.0 10 True
-    let wrappedApp = circuitBreakerMiddleware cb app
-    run 8080 wrappedApp
+    let wrappedApp = circuitBreakerMiddleware cb (proxyApp manager)
+    run 8081 wrappedApp
